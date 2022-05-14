cdac9dfd9660b326b61b1962174c0a1cac185ae0
黄立铭 <frank@zasoft.cn>
2022-05-14 17:32:58 +0800

    add admin menu and change username
    to frank.admin

--- ApiDemoApp/Repositories/UserRepository.cs
+++ ApiDemoApp/Repositories/UserRepository.cs
@@ -6,8 +6,8 @@ namespace ApiDemoApp.Repositories
     {
         public static List<User> Users = new()
         {
-            new() { Username = "luke_admin", EmailAddress = "luke.admin@email.com", Password = "MyPass_w0rd", GivenName = "Luke", Surname = "Rogers", Role = "Admin" },
-            new() { Username = "lydia_standard", EmailAddress = "lydia.standard@email.com", Password = "MyPass_w0rd", GivenName = "Elyse", Surname = "Burton", Role = "User" },
+            new() { Username = "frank_admin", EmailAddress = "frank.admin@email.com", Password = "PassWord", GivenName = "Frank", Surname = "Huang", Role = "Admin" },
+            new() { Username = "frank_standard", EmailAddress = "frank.standard@email.com", Password = "PassWord", GivenName = "Elyse", Surname = "Burton", Role = "User" },
         };
     }
 }
